acd05ef3d0c2d68de020dd7cad28d2205d7b44c3
Omma Sharika Shama <ommasharika@yahoo.com>
2024-11-23 09:06:59 +0600

    Different variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String message = "Hello, World!"; // String variable
+        int number = 42;                 // Integer variable
+        double pi = 3.14159;             // Double variable
+        char letter = 'H';               // Character variable
+        boolean isJavaFun = true;        // Boolean variable
+
+        System.out.println(message);
+        System.out.println("Number: " + number);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("First Letter: " + letter);
+        System.out.println("Is Java fun? " + isJavaFun);
     }
 }
